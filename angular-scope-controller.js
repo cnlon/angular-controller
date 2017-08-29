7bcab5ec31004e703507c5a1e93f66872f7ab79d
lon <longhaohe@gmail.com>
2017-08-29 14:56:28 +0800

    Support watch

--- angular-scope-controller.js
+++ angular-scope-controller.js
@@ -10,44 +10,99 @@
 
     class ScopeController {
         constructor (...args) {
-            const {$inject, prototype} = this.constructor
-            if ($inject) {
-                $inject.forEach((key, index) => this[key] = args[index])
+            const {$inject: inject, prototype} = this.constructor
+            if (inject) {
+                inject.forEach((key, index) => this[key] = args[index])
             }
 
             const state = this.$onState && this.$onState(...args)
+            const scope = this.$scope
             if (state) {
-                if (this.$scope) {
+                if (scope) {
                     proxyState(this, state)
                 } else {
                     assignState(this, state)
                 }
             }
 
-            if (this.$scope) {
-                const descriptors = Object.getOwnPropertyDescriptors(prototype)
-                const toAssignDescriptors = {}
-                for (const [key, descriptor] of Object.entries(descriptors)) {
-                    if (key === 'constructor' || key.startsWith('$')) {
-                        continue
-                    }
-                    if (angular.isFunction(descriptor.value)) {
-                        descriptor.value = this::descriptor.value
-                    } else {
-                        if (angular.isFunction(descriptor.get)) {
-                            descriptor.get = this::descriptor.get
-                        }
-                        if (angular.isFunction(descriptor.set)) {
-                            descriptor.set = this::descriptor.set
-                        }
-                    }
-                    toAssignDescriptors[key] = descriptor
-                }
-                assign(this.$scope, toAssignDescriptors)
+            if (!scope) {
+                return
+            }
 
-                Object.defineProperty(this.$scope, '$$scopeController', {
-                    value: this
-                })
+            if (process.env.NODE_ENV === 'development') {
+                Object.defineProperty(scope, '$$scopeController', {value: this})
+            }
+
+            const descriptors = Object.getOwnPropertyDescriptors(prototype)
+            const toAssignDescriptors = {}
+            for (const [key, descriptor] of Object.entries(descriptors)) {
+                if (key === 'constructor' || key.startsWith('$')) {
+                    continue
+                }
+                if (angular.isFunction(descriptor.value)) {
+                    descriptor.value = this::descriptor.value
+                } else {
+                    if (angular.isFunction(descriptor.get)) {
+                        descriptor.get = this::descriptor.get
+                    }
+                    if (angular.isFunction(descriptor.set)) {
+                        descriptor.set = this::descriptor.set
+                    }
+                }
+                toAssignDescriptors[key] = descriptor
+            }
+            assign(scope, toAssignDescriptors)
+
+            const toWatch = prototype.$$toWatch
+            if (!toWatch) {
+                return
+            }
+            const unwatches = {}
+            for (const [key, options] of Object.entries(toWatch)) {
+                unwatches[key] = this.$watch(...options.expressions, options)
+            }
+            Object.defineProperty(this, '$$unwatches', {
+                value: unwatches
+            })
+        }
+
+        $watch (...args) {
+            const options = args.pop()
+            const expressions = args
+            let callback, deep, strict
+            if (angular.isFunction(options)) {
+                callback = this::options
+            } else {
+                callback = this::options.callback
+                deep = options.deep
+                strict = options.strict
+            }
+
+            const scope = this.$scope
+            if (expressions.length > 1) {
+                return scope.$watchGroup(expressions, callback)
+            }
+            const [expression] = expressions
+            if (deep) {
+                return scope.$watchCollection(expression, callback)
+            } else if (strict ) {
+                return scope.$watch(expression, callback, true)
+            } else {
+                return scope.$watch(expression, callback)
+            }
+        }
+
+        $unwatch (...names) {
+            const unwatches = this.$$unwatches
+            if (!unwatches) {
+                return
+            }
+            for (const name of names) {
+                const unwatch = unwatches[name]
+                if (unwatch) {
+                    unwatch()
+                    delete unwatches[name]
+                }
             }
         }
     }
@@ -141,6 +196,36 @@
         }
     }
 
+    function watch (...args) {
+        const expressions = args
+        const options = {
+            expressions,
+            callback: undefined,
+            deep: undefined,
+            strict: undefined
+        }
+        const lastArg = expressions[args.length - 1]
+        if (lastArg === true || lastArg === false) {
+            options.deep = lastArg
+            expressions.pop()
+        } else if (angular.isObject(lastArg)) {
+            options.deep = lastArg.deep
+            options.strict = lastArg.strict
+            expressions.pop
+        }
+        return function registerWatch (prototype, property, descriptor) {
+            options.callback = descriptor.value
+            if (!prototype['$$toWatch']) {
+                Object.defineProperty(prototype, '$$toWatch', {
+                    value: Object.create(null),
+                    configurable: true
+                })
+            }
+            prototype['$$toWatch'][property] = options
+        }
+    }
+
     exports.ScopeController = angular.ScopeController = ScopeController
-    exports.inject = angular.inject = inject
+    exports.inject = ScopeController.inject = inject
+    exports.watch = ScopeController.watch = watch
 }));
